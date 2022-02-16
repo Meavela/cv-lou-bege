afb2dddc4bfba86627adad25911639dd6ba00d52
Lou <lou.bege@epsi.fr>
2022-02-16 18:57:06 +0100

    🎨Make responsive appbar

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -3,10 +3,14 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Container from '@mui/material/Container';
 import '../App.css';
+import $ from 'jquery';
 import { Link } from "react-router-dom";
-import {Grid, Box} from '@mui/material';
+import {Grid, Box, Button} from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ThemeColor from '../screens/Theme';
 import Traduction from '../screens/Traduction';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
 
 const pages = [{"url": "/cv-lou-bege/", "title":"A propos"},
                 {"url": "/cv-lou-bege/formations", "title":"Formations"},
@@ -16,28 +20,68 @@ const pages = [{"url": "/cv-lou-bege/", "title":"A propos"},
             ];
 
 function Navigation() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
     return (
-        <AppBar className="App">
+        <AppBar className="AppBar">
             <Container maxWidth="xl">
-                <Toolbar disableGutters>
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }} >
+                <Toolbar disableGutters className='FullScreen'>
+                    <Box  sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }} >
                         <Grid container >
                             {pages.map((page) => (
                                     <Grid item xs={2} key={page.url}>
-                                        <Link to={page.url} className="componentNavigation">{page.title}</Link >
+                                        <Link to={page.url} className="AppBar-ComponentNavigation">{page.title}</Link >
                                     </Grid>
                             ))}
                         </Grid>
                     </Box>
                     
-                    <Grid item xs={2}>
+                    {/* <Grid item xs={2}>  */}
                         {/* <ThemeColor></ThemeColor> */}
                         {/* <Traduction></Traduction> */}
+                    {/* </Grid> */}
+                </Toolbar>
+                <Toolbar disableGutters className='ReduceScreen'>
+                    <Grid item xs={12}> 
+                        <Button variant="contained" style={{width: '20vw', color:"white", fontSize:'2.5vw', backgroundColor:"#373d49"}} onClick={handleMenu} aria-controls="menu-appbar" >
+                            Menu <ExpandMoreIcon fontSize="large"></ExpandMoreIcon>
+                            </Button>
                     </Grid>
+                    {/* <Grid item xs={2}> */}
+                        {/* <Traduction></Traduction> */}
+                    {/* </Grid> */}
+                    <Menu
+                        id="menu-appbar"
+                        anchorEl={anchorEl}
+                        anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                        }}
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                    >
+                        {pages.map((page) => (
+                            <MenuItem onClick={handleClose} key={page.url} ><Link to={page.url}>{page.title}</Link ></MenuItem>
+                        ))}
+                    </Menu>
                 </Toolbar>
             </Container>
         </AppBar>
     );
 }
+
+const styles = [{
+
+}];
 
 export default Navigation;
